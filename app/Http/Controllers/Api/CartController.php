becc16680dc8b3b5ffdeacfd0e934a00c99e57ec
tawhan695 <36127485+tawhan695@users.noreply.github.com>
2021-09-15 16:26:13 +0700

    Update CartController.php

--- app/Http/Controllers/Api/CartController.php
+++ app/Http/Controllers/Api/CartController.php
@@ -1,8 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api;
 
-use App\Models\Product;
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class CartController extends Controller
