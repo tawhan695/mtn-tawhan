86c8648d7c526c66de8841b5954463de6fadff9e
tawhan695 <36127485+tawhan695@users.noreply.github.com>
2021-11-26 12:23:02 +0700

    s

--- app/Models/User.php
+++ app/Models/User.php
@@ -56,7 +56,7 @@ class User extends Authenticatable
     // }
     public function Branch(){
         // echo $this->profile()->first();
-        return Branchs::where('id',auth()->user()->branchs_id())->first();
+        return Branchs::where('id',auth()->user()->branch_id())->first();
     }
     public function branch_id(){
        return Has_Branchs::where('user_id',auth()->user()->id)->first()->branchs_id;
